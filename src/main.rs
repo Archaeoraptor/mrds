3fbfc37c2ec97bacb663361b16de9e138587b27d
zhixi <zhangjk67@gmail.com>
2023-12-29 16:06:42 +0800

    shared state

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,38 @@
+use bytes::Bytes;
 use mini_redis::{Connection, Frame};
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     // Bind the listener to the address
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         // the second item contains the IP and port of the new connection.
         let (socket, _) = listener.accept().await.unwrap();
+
+        let db = db.clone();
+
+        println!("Accepted");
+
         // generate new task
         // move socket to new task
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    // a hashmap is used to store data
-    let mut db = HashMap::new();
 
     // socket
     let mut connection = Connection::new(socket);
@@ -32,12 +42,14 @@ async fn process(socket: TcpStream) {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
                 // the value is stored as Vec<u8>
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
-                Frame::Simple("Ok".to_string())
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
+                Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
